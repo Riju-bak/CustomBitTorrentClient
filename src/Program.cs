4b6306f036baa12d604c9e53aa9cc9dafc5f5bc4
Riju-bak <defaltsith@gmail.com>
2025-06-08 23:16:33 -0400

    decode bencoded lists

--- src/Program.cs
+++ src/Program.cs
@@ -23,43 +23,58 @@ public class Program
             // You can use print statements as follows for debugging, they'll be visible when running tests.
             Console.Error.WriteLine("Logs from your program will appear here!");
 
-            // Uncomment this line to pass the first stage
             var encodedValue = param;
-            if (Char.IsDigit(encodedValue[0]))
-            {
-                // Example: "5:hello" -> "hello"
-                var colonIndex = encodedValue.IndexOf(':');
-                if (colonIndex != -1)
-                {
-                    var strLength = int.Parse(encodedValue[..colonIndex]);
-                    var strValue = encodedValue.Substring(colonIndex + 1, strLength);
-                    Console.WriteLine(JsonSerializer.Serialize(strValue));
-                }
-                else
-                {
-                    throw new InvalidOperationException("Invalid encoded value: " + encodedValue);
-                }
-            }
-            else if (encodedValue[0]=='i')
-            {
-                int eIndex = encodedValue.IndexOf('e');
-                if (eIndex != -1 && eIndex == encodedValue.Length - 1)
-                {
-                    // i123e
-                    string strNum = encodedValue.Substring(1, eIndex - 1);
-                    Console.WriteLine(long.Parse(strNum));
-                }
-                else throw new InvalidOperationException($"Invalid encoded value: {encodedValue}");
-            }
-            else
-            {
-                throw new InvalidOperationException("Unhandled encoded value: " + encodedValue);
-            }
+            int index = 0;
+            var decoded = DecodeEncodedValue(encodedValue, ref index);
+            Console.WriteLine(JsonSerializer.Serialize(decoded));
         }
         else
         {
             throw new InvalidOperationException($"Invalid command: {command}");
         }
-    }    
+    }
+
+    private static object DecodeEncodedValue(string input, ref int index)
+    {
+        char current = input[index];
+
+        if (char.IsDigit(current))
+        {
+            // Parse string: <length>:<string>
+            int colonIndex = input.IndexOf(':', index);
+            int length = int.Parse(input.Substring(index, colonIndex - index));
+            index = colonIndex + 1;
+            string strValue = input.Substring(index, length);
+            index += length;
+            return strValue;
+        }
+        else if (current == 'i')
+        {
+            // Parse integer: i<digits>e
+            index++; // Skip 'i'
+            int endIndex = input.IndexOf('e', index);
+            string numStr = input.Substring(index, endIndex - index);
+            long number = long.Parse(numStr);
+            index = endIndex + 1;
+            return number;
+        }
+        else if (current == 'l')
+        {
+            // Parse list: l<values>e
+            index++; // Skip 'l'
+            var list = new List<object>();
+            while (input[index] != 'e')
+            {
+                list.Add(DecodeEncodedValue(input, ref index));
+            }
+            index++; // Skip 'e'
+            return list;
+        }
+        else
+        {
+            throw new InvalidOperationException("Unhandled encoded value starting at: " + input[index]);
+        }
+    }
+    
 }
 
